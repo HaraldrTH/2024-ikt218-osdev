22044758e44aa278a72342976133d4e5dcf96be9
Filip <Acerradi@gmail.com>
2024-04-22 14:18:29 +0000

    keyboardv0.6

--- src/34_amigOS/src/lib/src/keyboard.c
+++ src/34_amigOS/src/lib/src/keyboard.c
@@ -7,15 +7,9 @@ init_scancodes(){
     insert(scancodes, "30", "A");
 }
 void keyboard_handler(){
-    printf("Keyboard");
     char* scancode = '\0';
     char* character = '\0';
-    while (1)
-    {
-        int input = inb(0x60);
-        int_to_string(input, scancode);
-        character = get(scancodes, scancode);
-        printf(character);
-        character = "\0";
-    }
+    int input = inb(0x60);
+    int_to_string(input, scancode);
+    character = get(scancodes, scancode);
 }
