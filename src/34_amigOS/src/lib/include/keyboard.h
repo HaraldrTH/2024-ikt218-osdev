7e6818fb0ca0f72df350b675ec2167aab02dda10
Filip <Acerradi@gmail.com>
2024-04-22 12:58:42 +0000

    keyboardv0.5

--- src/34_amigOS/src/lib/include/keyboard.h
+++ src/34_amigOS/src/lib/include/keyboard.h
@@ -1,5 +1,3 @@
 #pragma once
 #include "../include/dict.h"
-Dictionary* scancode_table;
-void init_scancode_table();
 void keyboard_handler();
--- src/34_amigOS/src/lib/src/keyboard.c
+++ src/34_amigOS/src/lib/src/keyboard.c
@@ -4,10 +4,6 @@
 
 
 
-void init_scancode_table(Dictionary* dict){
-    create_pair("1f", "a");
-    insert(scancode_table, "1f", "a");
-}
 
 void keyboard_handler(){
     printf("Keyboard");
@@ -16,7 +12,6 @@ void keyboard_handler(){
     while (1)
     {
         int_to_string(inb(0x60), scancode);
-        character = get(scancode_table, scancode);
         printf(character);
     }
 }
