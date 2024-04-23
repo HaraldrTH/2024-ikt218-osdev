bae611e1b8f1f2c2cff0c416d605a790e1bd7b8e
HaraldrTH <haraldth@student.uia.no>
2024-04-23 13:50:01 +0000

    Assignment 4. PIT completed

--- src/34_amigOS/src/lib/src/pit.c
+++ src/34_amigOS/src/lib/src/pit.c
@@ -1,46 +1,60 @@
 #include "../include/pit.h"
 #include "../include/libc/stdint.h"
 #include "../include/stdlib.h"
+#include "../include/IRQ.h"
+#include "../include/ports.h"
 
 uint32_t tick = 0;
 
 static void timer_callback(){
     tick++;
-    printf("Tick: ");
-    char* arg = int_to_string(tick, 0);
-    printf(arg);
-    printf("\n");
+    if (tick % 1000 == 0){
+        printf("Tick: ");
+        printf(int_to_string(tick, 0));
+        printf("\n");
+    }
 };
 
+uint32_t get_current_tick(){
+    return tick;
+};
 
 void init_pit(){
-    return 0;
+    // Register our timer callback
+    register_interrupt_handler(32, &timer_callback);
+
+    uint32_t divisor = DIVIDER;
+
+    // Send the command byte.
+    outb(0x43,0x36);
+
+    // Divisor has to be sent byte-wise, so split here into upper/lower bytes.
+    uint8_t l = (uint8_t)(divisor & 0xFF);
+    uint8_t h = (uint8_t)((divisor>>8) & 0xFF);
+
+    outb(0x40, l);
+    outb(0x40, h);
 };
 
 void sleep_interrupt(uint32_t milliseconds){
-    /*
-    start_tick = get_current_tick();
-	ticks_to_wait = milliseconds * TICKS_PER_MS;
-	elapsed_ticks = 0;
-	while(elapsed_ticks < than ticks_to_wait){
-		while (get_current_tick() = start_tick + elapsed_ticks){
+    uint32_t start_tick = get_current_tick();
+	uint32_t ticks_to_wait = milliseconds * TICKS_PER_MS;
+	int elapsed_ticks = 0;
+while((int)elapsed_ticks < (int)ticks_to_wait){
+    while (get_current_tick() == start_tick + elapsed_ticks){
             continue; // Do nothing
         }
 		elapsed_ticks++;
     }
-    */
 };
 
 void sleep_busy(uint32_t milliseconds){
-    /*
-    current_tick = get_current_tick();
-	ticks_to_wait = milliseconds * TICKS_PER_MS;
-	end_ticks = current_tick + ticks_to_wait;
-	While (current_tick < end_ticks){
-		Execute the following assembly instructions:
-			i. Enable interrupts (sti)
-			ii. Halt the CPU until the next interrupt (hlt)
-		currrent_tick = get_current_tick();
+    uint32_t current_tick = get_current_tick();
+	uint32_t ticks_to_wait = milliseconds * TICKS_PER_MS;
+	uint32_t end_ticks = current_tick + ticks_to_wait;
+	while(current_tick < end_ticks){
+		asm volatile ("sti\n\t"
+                      "hlt\n\t");
+		current_tick = get_current_tick();
     }
-    */
 };
